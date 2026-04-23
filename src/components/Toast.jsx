68f539dcb7db241e82c5fec5c3c4012bb63baf05
mihxlko <a7.mihalko@gmail.com>
2026-04-23 08:12:12 -0400

    changed styling of toast. made small spacing adjustments to settings.

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -3,9 +3,9 @@ export default function Toast({ message = 'Moved to Trash', action, onAction }) 
     <div className="toast-container">
       <div className="toast">
         <span className="toast-text">{message}</span>
+        {action && <div className="toast-divider" />}
         {action && <button className="toast-restore-btn" onClick={onAction}>{action}</button>}
       </div>
-      <div className="toast-progress" />
     </div>
   )
 }
